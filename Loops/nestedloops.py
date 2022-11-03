f23de19addc3be1ad4490a801fc4a1cf2e879da2
harrtedwin <mkihara.edwin@gmail.com>
2022-11-04 01:45:01 +0300

    changes

--- Loops/nestedloops.py
+++ Loops/nestedloops.py
@@ -9,7 +9,7 @@ for row in range(5):
     if row % 2 == 0:
         for  column in range(1,6):
             if column % 2 == 1:
-                if column % 2 != 5:
+                if column != 5:
                     print(" ", end="")
                 else:
                     print(" ")
